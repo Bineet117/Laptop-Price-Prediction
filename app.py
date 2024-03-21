1ea6790c2730922d2d85e0a32755327164ca48b0
Bineet117 <bineetgupta117@gmail.com>
2024-03-21 13:29:20 +0530

    h

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import pickle
+st.title("Laptop Price Prediction")
+
+# import the model
+# pipe = pickle.load(open("model1.pkl" ,"rb"))
+df = pickle.load(open("data.pkl" ,"rb"))
+
+# Brand
+Company = st.selectbox("Brand",df["Company"].unique())
+
+# type of laptop
+TypeName = st.selectbox("TypeName",df["TypeName"].unique())
+RAM = st.selectbox("Ram(in GB)",df["Ram"].unique())
+Weight = st.number_input("Weight of the Laptop")
+TouchScreen = st.selectbox("TouchScreen",["No","Yes"])
+IPS = st.selectbox("IPS",["No","Yes"])
+# RAM = st.selectbox("TypeName",df["TypeName"].unique())
